563d77e04d3d0fc9e48e62091482b874610b1859
bwllaming <128721524+bwllaming@users.noreply.github.com>
2023-05-13 12:38:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log('App listening on port 3000');
 });
 
